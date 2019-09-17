cb56e7bb59f9d0413fd3ea213b0c9a18a0159f18
Diana Epureanu <epureanudiana13@gmail.com>
2019-09-17 10:44:55 +0200

    Exercise 4

---
+++ color_constancy/AWB.m
@@ -0,0 +1,24 @@
+
+clear
+clc
+close all
+% read image and extract dimensions
+I = imread('awb.jpg');
+[rows, columns, ~] = size(I);
+
+%extract color channels
+r = I(:, :, 1);
+g = I(:, :, 2);
+b = I(:, :, 3);
+
+%adjust pixels per channel
+r_c = 128/mean(r, 'all')*r;
+g_c = 128/mean(g, 'all')*g;
+b_c = 128/mean(b, 'all')*b;
+corrected_image = cat(3, r_c, g_c, b_c);
+
+figure()
+subplot(1,2,1) 
+imshow(I)
+subplot(1,2,2)
+imshow(corrected_image)
