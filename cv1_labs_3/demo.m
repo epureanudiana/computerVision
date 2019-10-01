106665ac3bbdc4544884e924a6b71407887684aa
maria.diea <diea.maria.alexandra@gmail.com>
2019-10-01 17:07:58 +0200

    Part2Q1 show both plots

--- cv1_labs_3/demo.m
+++ cv1_labs_3/demo.m
@@ -1,6 +1,17 @@
 im1 = imread('sphere1.ppm');
 im2 = imread('sphere2.ppm');
 
+figure(2);
+subplot(121);
 imshow(im1);
 hold on;
 [v_x, v_y] = lucas_kanade(im1,im2);
+title("Sphere");
+
+im1 = imread('synth1.pgm');
+im2 = imread('synth2.pgm');
+subplot(122);
+imshow(im1);
+hold on;
+[v_x, v_y] = lucas_kanade(im1,im2);
+title("Synth");
