e3354d6cfd5386c5e05819b8ba8ffa1aac7df347
Diana Epureanu <epureanudiana13@gmail.com>
2019-10-01 16:43:34 +0200

    ex 1 done

--- cv1_labs_3/harris_corner_detector_test.m
+++ cv1_labs_3/harris_corner_detector_test.m
@@ -1,11 +1,35 @@
 % read image
-%img = imread("person_toy/00000001.jpg");
-img = imread("person_toy/00000001.jpg");
+img_pingpong = imread("pingpong/0000.jpeg");
+[H, r, c, Ix, Iy] = harris_corner_detector(img_pingpong);
 
-[r, c] = harris_corner_detector(img);
+img_toy = imread("person_toy/00000001.jpg");
+[H2, r2, c2, Ix2, Iy2] = harris_corner_detector(img_toy);
+
+
 figure("Name", "Harris corner detection results");
-imshow(img);
+subplot(3,2,1);
+imshow(img_pingpong);
 axis on
 hold on;
-% Plot cross at row 100, column 50
-plot(r,c, 'r+', 'MarkerSize', 20, 'LineWidth', 2);
+% Plot corner points on the original image too
+plot(r,c, 'b+', 'MarkerSize', 7, 'LineWidth', 2);
+title("Original image + corner points")
+subplot(3,2,2);
+imshow(img_toy);
+axis on
+hold on;
+% Plot corner points on the original image too
+plot(r2,c2, 'b+', 'MarkerSize', 7, 'LineWidth', 2);
+title("Original image + corner points")
+subplot(3,2,3);
+imshow(Ix);
+title("Ix derivative");
+subplot(3,2,4);
+imshow(Ix2);
+title("Ix derivative");
+subplot(3,2,5);
+imshow(Iy);
+title("Iy derivative")
+subplot(3,2,6);
+imshow(Iy2);
+title("Iy derivative");
